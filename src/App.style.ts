4a598bf2f002d8516c6dd77d4b5ae2985e7b29b6
dolidius <dolidius@gmail.com>
2021-05-26 01:07:22 +0200

    styled-components setup

---
+++ src/App.style.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 10px;
+    }
+
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 
+import GlobalStyle from './App.style';
+
 function App() {
     return (
         <Provider store={store}>
+            <GlobalStyle />
             <div>
                 test
             </div>
