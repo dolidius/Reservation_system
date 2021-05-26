cb26de3107ad07a370e692e8e04340c2fb0b83c5
dolidius <dolidius@gmail.com>
2021-05-26 12:54:53 +0200

    added basic form components

--- src/components/Screens/Home/Home.tsx
+++ src/components/Screens/Home/Home.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import { Form, InputNumber, Button, Checkbox } from 'antd';
+
 import { ViewportContainer } from '../../../styles/Layout/Container.style';
 
 const Home = () => {
+
+    const [form] = Form.useForm();
+
+    const onFinish = (values: any) => {
+        console.log(values);
+    }
+
     return (
         <ViewportContainer centered>
-            Home
+            <Form form={form} name="tickets" onFinish={onFinish}>
+                <Form.Item name="tickets_number" rules={[{ required: true }]}>
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item>
+                    <Checkbox>
+                        Czy miejsca mają być obok siebie?
+                    </Checkbox>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         </ViewportContainer>
     )
 }
