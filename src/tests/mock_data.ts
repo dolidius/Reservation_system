1e1f321a8e570eb54e8f9fbb1afab61ed8aa3f9c
dolidius <dolidius@gmail.com>
2021-05-31 18:47:58 +0200

    added basic templates for testing

---
+++ src/tests/mock_data.ts
@@ -0,0 +1,370 @@
+export const seats =  [
+    {
+      "id": "s02",
+      "cords": {
+        "x": 0,
+        "y": 2
+      },
+      "reserved": false
+    },
+    {
+      "id": "s03",
+      "cords": {
+        "x": 0,
+        "y": 3
+      },
+      "reserved": false
+    },
+    {
+      "id": "s04",
+      "cords": {
+        "x": 0,
+        "y": 4
+      },
+      "reserved": false
+    },
+    {
+      "id": "s06",
+      "cords": {
+        "x": 0,
+        "y": 6
+      },
+      "reserved": false
+    },
+    {
+      "id": "s07",
+      "cords": {
+        "x": 0,
+        "y": 7
+      },
+      "reserved": false
+    },
+    {
+      "id": "s08",
+      "cords": {
+        "x": 0,
+        "y": 8
+      },
+      "reserved": true
+    },
+    {
+      "id": "s09",
+      "cords": {
+        "x": 0,
+        "y": 9
+      },
+      "reserved": true
+    },
+    {
+      "id": "s011",
+      "cords": {
+        "x": 0,
+        "y": 11
+      },
+      "reserved": false
+    },
+    {
+      "id": "s012",
+      "cords": {
+        "x": 0,
+        "y": 12
+      },
+      "reserved": false
+    },
+    {
+      "id": "s013",
+      "cords": {
+        "x": 0,
+        "y": 13
+      },
+      "reserved": false
+    },
+    {
+      "id": "s014",
+      "cords": {
+        "x": 0,
+        "y": 14
+      },
+      "reserved": false
+    },
+    {
+      "id": "s12",
+      "cords": {
+        "x": 1,
+        "y": 2
+      },
+      "reserved": false
+    },
+    {
+      "id": "s13",
+      "cords": {
+        "x": 1,
+        "y": 3
+      },
+      "reserved": false
+    },
+    {
+      "id": "s14",
+      "cords": {
+        "x": 1,
+        "y": 4
+      },
+      "reserved": false
+    },
+    {
+      "id": "s16",
+      "cords": {
+        "x": 1,
+        "y": 6
+      },
+      "reserved": false
+    },
+    {
+      "id": "s17",
+      "cords": {
+        "x": 1,
+        "y": 7
+      },
+      "reserved": false
+    },
+    {
+      "id": "s18",
+      "cords": {
+        "x": 1,
+        "y": 8
+      },
+      "reserved": false
+    },
+    {
+      "id": "s19",
+      "cords": {
+        "x": 1,
+        "y": 9
+      },
+      "reserved": false
+    },
+    {
+      "id": "s111",
+      "cords": {
+        "x": 1,
+        "y": 11
+      },
+      "reserved": false
+    },
+    {
+      "id": "s112",
+      "cords": {
+        "x": 1,
+        "y": 12
+      },
+      "reserved": false
+    },
+    {
+      "id": "s113",
+      "cords": {
+        "x": 1,
+        "y": 13
+      },
+      "reserved": false
+    },
+    {
+      "id": "s114",
+      "cords": {
+        "x": 1,
+        "y": 14
+      },
+      "reserved": false
+    },
+    {
+      "id": "s22",
+      "cords": {
+        "x": 2,
+        "y": 2
+      },
+      "reserved": false
+    },
+    {
+      "id": "s23",
+      "cords": {
+        "x": 2,
+        "y": 3
+      },
+      "reserved": false
+    },
+    {
+      "id": "s24",
+      "cords": {
+        "x": 2,
+        "y": 4
+      },
+      "reserved": false
+    },
+    {
+      "id": "s26",
+      "cords": {
+        "x": 2,
+        "y": 6
+      },
+      "reserved": false
+    },
+    {
+      "id": "s27",
+      "cords": {
+        "x": 2,
+        "y": 7
+      },
+      "reserved": false
+    },
+    {
+      "id": "s28",
+      "cords": {
+        "x": 2,
+        "y": 8
+      },
+      "reserved": false
+    },
+    {
+      "id": "s29",
+      "cords": {
+        "x": 2,
+        "y": 9
+      },
+      "reserved": false
+    },
+    {
+      "id": "s211",
+      "cords": {
+        "x": 2,
+        "y": 11
+      },
+      "reserved": false
+    },
+    {
+      "id": "s212",
+      "cords": {
+        "x": 2,
+        "y": 12
+      },
+      "reserved": false
+    },
+    {
+      "id": "s213",
+      "cords": {
+        "x": 2,
+        "y": 13
+      },
+      "reserved": true
+    },
+    {
+      "id": "s214",
+      "cords": {
+        "x": 2,
+        "y": 14
+      },
+      "reserved": true
+    },
+    {
+      "id": "s30",
+      "cords": {
+        "x": 3,
+        "y": 0
+      },
+      "reserved": true
+    },
+    {
+      "id": "s31",
+      "cords": {
+        "x": 3,
+        "y": 1
+      },
+      "reserved": true
+    },
+    {
+      "id": "s32",
+      "cords": {
+        "x": 3,
+        "y": 2
+      },
+      "reserved": false
+    },
+    {
+      "id": "s33",
+      "cords": {
+        "x": 3,
+        "y": 3
+      },
+      "reserved": false
+    },
+    {
+      "id": "s34",
+      "cords": {
+        "x": 3,
+        "y": 4
+      },
+      "reserved": false
+    },
+    {
+      "id": "s36",
+      "cords": {
+        "x": 3,
+        "y": 6
+      },
+      "reserved": true
+    },
+    {
+      "id": "s37",
+      "cords": {
+        "x": 3,
+        "y": 7
+      },
+      "reserved": false
+    },
+    {
+      "id": "s38",
+      "cords": {
+        "x": 3,
+        "y": 8
+      },
+      "reserved": false
+    },
+    {
+      "id": "s39",
+      "cords": {
+        "x": 3,
+        "y": 9
+      },
+      "reserved": true
+    },
+    {
+      "id": "s311",
+      "cords": {
+        "x": 3,
+        "y": 11
+      },
+      "reserved": true
+    },
+    {
+      "id": "s312",
+      "cords": {
+        "x": 3,
+        "y": 12
+      },
+      "reserved": true
+    },
+    {
+      "id": "s313",
+      "cords": {
+        "x": 3,
+        "y": 13
+      },
+      "reserved": false
+    },
+    {
+      "id": "s314",
+      "cords": {
+        "x": 3,
+        "y": 14
+      },
+      "reserved": false
+    },
+]
